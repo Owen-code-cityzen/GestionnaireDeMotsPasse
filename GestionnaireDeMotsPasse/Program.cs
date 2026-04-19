85ad4d267f19939a97a7e41f13893469e0d1ffe4
Owen <ocdjelloh@gmail.com>
2026-04-19 02:12:39 +0000

    Add project files.

---
+++ GestionnaireDeMotsPasse/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;   
+using GestionnaireDeMotsPasse;
+
+int LongueurDePasse = outil.DemanderNombrePositifNonNul("Veuillez choisir la longueur de votre mot de passe: ");
+Console.WriteLine();
+
+int ChoixAlphabet = outil.DemanderNombreEntre("Veuillez choisir une option de mots de passes \n" +
+    "1- Lettres minuscules \n" +
+    "2- Lettres minuscules et majuscules \n" +
+    "3- Lettres minuscules et majuscules et chiffres \n" +
+    "4- Lettres minuscules, majuscules, chiffres et caractères spéciaux \n" +
+    "Votre choix: ", 1, 4);
+
+Console.WriteLine();
+int nombreDeMotsDePasse = outil.DemanderNombrePositifNonNul("Combien de mots de passe voulez-vous générer ? : ");
+Console.WriteLine();
+
+string minuscule = "abcdefghijklmnopqrstuvwxyz";
+string majuscule = minuscule.ToUpper();
+string chiffre = "1234567890";
+string caracteres_speciaux = "$%^&*!";
+string Alphabet;
+Random rand = new Random();
+
+if (ChoixAlphabet == 1)
+    Alphabet = minuscule;
+else if (ChoixAlphabet == 2)
+    Alphabet = minuscule + majuscule;
+else if (ChoixAlphabet == 3)
+    Alphabet = minuscule + chiffre + majuscule;
+else
+    Alphabet = minuscule + chiffre + majuscule + caracteres_speciaux;
+
+int LongueurAlphabet = Alphabet.Length;
+
+for (int j = 0; j < nombreDeMotsDePasse; j++)
+{
+    string MotDePasse = "";
+
+    for (int i = 0; i < LongueurDePasse; i++)
+    {
+        int index = rand.Next(0, LongueurAlphabet);
+        MotDePasse += Alphabet[index];
+    }
+
+    Console.WriteLine($"Mot de passe {j + 1} : {MotDePasse}");
+}
---
+++ GestionnaireDeMotsPasse/outil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionnaireDeMotsPasse
+{
+    internal static class outil
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+        public static int DemanderNombre(string question)
+        {
+            int Nombre = 0;
+
+            while (true)
+            {
+                Console.Write(question);
+                string NomDmd = Console.ReadLine();
+                try
+                {
+                    Nombre = int.Parse(NomDmd);
+                    return Nombre;
+                }
+                catch
+                {
+                    Console.WriteLine("Entrer un nombre");
+                }
+            }
+        }
+
+       public static int DemanderNombreEntre(string question, int Min, int Max)
+        {
+            int nombre = DemanderNombre(question);
+
+            if (nombre >= Min && nombre <= Max)
+            {
+                return nombre;
+            }
+            Console.WriteLine($"Erreur veuillez entrer un nombre entre {Min} et {Max}");
+            return DemanderNombreEntre(question, Min, Max);
+        }
+
+    }
+}
